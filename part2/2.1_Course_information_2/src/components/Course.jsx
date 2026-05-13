649ad91faecb4ea9bc03aee24944c375ea66f208
Teemu Ikonen <teemuikonenrlive.fi>
2026-05-13 22:43:47 +0300

    2.3 complete

--- part2/2.1_Course_information_2/src/components/Course.jsx
+++ part2/2.1_Course_information_2/src/components/Course.jsx
@@ -1,12 +1,14 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = (course) => {
-    
+
 return(
     <>
     <Header course={course}></Header>
     <Content parts={course.course.parts}></Content>
+    <Total parts={course.course.parts}></Total>
     </>
 )
 }
--- part2/2.1_Course_information_2/src/components/Total.jsx
+++ part2/2.1_Course_information_2/src/components/Total.jsx
@@ -1,8 +1,11 @@
 const Total = ({parts}) => {
-    
+
+const initValue = 0
+const total = parts.reduce((s, p) => s + p.exercises, initValue, );
+
 return ( 
     <>
-        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+        <strong>total of {total} exercises</strong>
     </>
     )
 }
