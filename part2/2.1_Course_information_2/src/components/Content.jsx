dd52cfd329b8c7ffd908a07018dba0d929ed483b
Teemu Ikonen <teemuikonenrlive.fi>
2026-05-13 23:04:42 +0300

    2.5 complete

--- part2/2.1_Course_information_2/src/components/Content.jsx
+++ part2/2.1_Course_information_2/src/components/Content.jsx
@@ -3,13 +3,11 @@ import Part from './Part'
 const Content = ({parts}) => {
     
 return ( 
-    <>
         <>
         {parts.map(part => 
         <Part key={part.id} part={part}/>
         )}
         </>
-    </>
     )
 }
 export default Content
