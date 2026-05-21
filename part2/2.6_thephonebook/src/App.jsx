b462a88dbacb2a25824cfb1ffe031b48fd1b1ab8
Teemu Ikonen <teemuikonenrlive.fi>
2026-05-21 09:36:31 +0300

    2.6 step 6 complete

--- part2/2.6_thephonebook/src/App.jsx
+++ part2/2.6_thephonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({searchField, onChange}) => {
   return(
@@ -49,15 +50,23 @@ const Person = ({person}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([ 
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchField, setSearchField] = useState('')
-  const [searchResults, setSearchResults] = useState(persons)
+  const [searchResults, setSearchResults] = useState([])
+
+  useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+      setSearchResults(response.data)
+    })
+}, [])
+
 
   const addPerson = (event) => {
     event.preventDefault()
