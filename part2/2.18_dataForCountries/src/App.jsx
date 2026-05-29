df1ed56fce4f98d41053347f2efcf5d24daf7f48
Teemu Ikonen <teemuikonenrlive.fi>
2026-05-29 16:27:02 +0300

    2.20 step 3 complete

--- part2/2.18_dataForCountries/src/App.jsx
+++ part2/2.18_dataForCountries/src/App.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react'
 import finland from './finland'
 import axios from 'axios'
 
+
 const Filter = ({searchField, onChange}) => {
   return(
     <>
@@ -10,19 +11,34 @@ const Filter = ({searchField, onChange}) => {
   )
 }
 
-const Countries = ({searchResults, countryCount, isSelected}) => {
+const Weather = ({country, weatherData}) => {
+
+  if (weatherData.length != 0) {
+     return(
+    <>
+    <h2>Weather in {country.capital}</h2>
+    <p>Temperature {weatherData.main.temp} Celsius</p>
+    <img src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}.png`}></img>
+    <p>Wind {weatherData.wind.speed} m/s</p>
+    </>
+  ) 
+  }
+}
+
+const Countries = ({searchResults, countryCount, isSelected, weatherData}) => {
       return(
     <>
       {searchResults.map(country =>   
       <Country key={country.name.common} country={country} 
-      countryCount={countryCount} isSelected={isSelected}></Country>
+      countryCount={countryCount} isSelected={isSelected} weatherData={weatherData}></Country>
       )}
     </>
   )
 }
 
-const Country = ({country, countryCount, isSelected}) => {
+const Country = ({country, countryCount, isSelected, weatherData}) => {
 
+  
   let languages = []
   try {
   languages = Object.values(country.languages)
@@ -48,6 +64,7 @@ const Country = ({country, countryCount, isSelected}) => {
       )}  
     </ul>
     <img src={country.flags.svg} width="200" height="100"></img>
+    <Weather country={country} weatherData={weatherData}></Weather>
     </> 
   )}
   else if (countryCount <= 10) {
@@ -61,7 +78,7 @@ const Country = ({country, countryCount, isSelected}) => {
 
 function App() {
 
-  const defaultCountry = finland
+
 
   const [countries, setCountries] = useState([])
   const [countryCount, setCountryCount] = useState(null)
@@ -69,22 +86,38 @@ function App() {
   const [searchResults, setSearchResults] = useState([])
   const [alert, setAlert] = useState('')
   const [isSelected, setIsSelected] = useState('')
+  const [weatherData, setWeatherData] = useState([])
 
   useEffect(() => {
     axios
     .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
     .then(response => {
-      setCountries(response.data)
+      setCountries(response.data)     
     })
     .catch(error => {
       console.log("GET error:", error);  
     })
   }, [])
+
+  const getWeatherData = (target) => {
+    console.log("weatherData: ",target);
+
+    axios
+    .get(`http://api.openweathermap.org/data/2.5/weather?q=${target[0].capital},${target[0].cca2}&units=metric&APPID=${import.meta.env.VITE_OPENWEATHER_API_KEY}`)
+    .then(response => {
+      setWeatherData(response.data)  
+      console.log("weather response data: ",response.data);  
+      })
+    .catch(error => {
+      console.log("Weather GET error: ", error);  
+    })
+  }
   
   const handleSelection = (ccn3) => {
     const result = countries.filter((country) => country.ccn3 === ccn3)
     setSearchResults(result)
     setCountryCount(1)
+    getWeatherData(result)
   }
 
   const handleSearchChange = (event) => {
@@ -114,7 +147,7 @@ function App() {
       <Filter searchField={searchField} onChange={handleSearchChange}></Filter>
       <div>{alert}</div>
       <Countries searchResults={searchResults} countryCount={countryCount} 
-      isSelected={handleSelection}></Countries>
+      isSelected={handleSelection} weatherData={weatherData}></Countries>
     </>
   )
 }
