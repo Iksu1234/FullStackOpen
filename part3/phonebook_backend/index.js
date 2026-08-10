0b7b7925c673cb1b038228a3869e11716ead9148
Teemu Ikonen <teemuikonenrlive.fi>
2026-08-10 12:00:57 +0300

    3.9 step 9 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('post', (res) => JSON.stringify(res.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
